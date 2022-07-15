454ba4e0cecf7c6c29cd13843d8344d52a1544f3
Aliitss <alebolivarparq@gmail.com>
2022-07-14 22:20:56 -0300

    sql

--- 03-sequelize/homework/db/models/Ability.js
+++ 03-sequelize/homework/db/models/Ability.js
@@ -2,6 +2,24 @@ const { DataTypes } = require('sequelize');
 
 module.exports = sequelize => {
   sequelize.define('Ability', {
-
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: "name_mana_cost",
+    },
+    description: {
+      type: DataTypes.TEXT,
+    },
+    mana_cost:{
+      type: DataTypes.FLOAT,
+      allowNull: false,
+      unique: "name_mana_cost",
+    }
   })
 }
+
+/* Ability
+name*: string
+description: text
+mana_cost*: float
+La combinación "name" + "mana_cost" debe ser única. */
